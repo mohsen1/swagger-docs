3318f1133d785930ed3bee936d6c6138cbbe4d32
Mohsen Azimi <msn.azi@gmail.com>
2015-02-05 16:48:02 -0800

    Add loading mechanism to main controller

--- docs/config.md
+++ docs/config.md
@@ -7,7 +7,37 @@ provided Swagger Docs will use it's own defaults. You can find default [`config.
 
 #### Available configurations in `config.json`
 
-
 ##### `loadSwaggerFrom`
 A relative or absolute URL of Swagger document you want to show in Swagger Docs. 
 Swagger document can be in YAML or JSON.
+
+
+#### Embedding configuration object
+To speed up the startup speed you can embed the configuration object. Instead of serving configuration object from your server you can put a `<script>` tag in `<head>` of `index.html` and provide the configuration.
+This script should define `swaggerDocsConfiguration` object that has configuration options in it.
+
+###### Example
+```html
+<head>
+  <script>
+    window.swaggerDocsConfiguration = {
+      loadSwaggerFrom: './my-swagger-doc.json'
+    };
+  </script>
+</head>
+```
+
+#### Embedding Swagger specs object
+You can also embed swagger specs object to speed up the startup time. You need to define `swaggerDocsSpecs` variable globally. It's best to put this code in `<head>` of `index.html`
+
+###### Example
+```html
+<head>
+  <script>
+    window.swaggerDocsSpecs = {
+      info: {/* ... */},
+      paths: {/* ... */}
+    };
+  </script>
+</head
+```
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,13 +2,98 @@
 /*jshint esnext: true */
 
 class MainCtrl {
-  constructor ($http) {
+  constructor ($http, $rootScope) {
 
-    // get configuration file
-    $http.get('./config.json').then((resp)=>console.log(resp.data));
+    // Initialize rendering of the app by assigning $rootScope variables
+    getConfigs();
+
+    /*
+     * Gets configurations from embedded script or remote file
+    */
+    function getConfigs() {
+
+      // use embedded configurations if it's available
+      if (angular.isObject(window.swaggerDocsConfiguration)) {
+        assignConfigs(window.swaggerDocsConfiguration);
+        return;
+      }
+
+      // get configuration file remotely
+      $http.get('./config.json').then(
+        function(resp) {
+
+          if (!angular.isObject(resp.data)) {
+            throw new Error('Configuration should be an object.');
+          }
+
+          assignConfigs(resp.data);
+        },
+        function() {
+          throw new Error('Failed to load configurations.');
+        }
+      );
+    }
+
+    /*
+     * Gets Swagger specs from embedded script or remote file
+    */
+    function getSwagger() {
+
+      // if specs is embedded in the document, use them
+      if (angular.isObject(window.swaggerDocsSpecs)) {
+        assignSwaggerSpecs(window.swaggerDocsSpecs);
+        return;
+      }
+
+      // get specs file remotely
+      $http.get($rootScope.config.loadSwaggerFrom).then(
+        function(resp) {
+
+          // if response data is string, it could be YAML.
+          if (angular.isString(resp.data)) {
+            const specs = window.jsyaml.load(resp.data);
+
+            if (!angular.isObject(specs)) {
+              throw new Error('Failed to parse Swagger YAML.');
+            }
+
+            assignSwaggerSpecs(specs);
+            return;
+          }
+
+          if (!angular.isObject(resp.data)) {
+            throw new Error('Swagger specs should be an object.');
+          }
+
+          assignSwaggerSpecs(resp.data);
+        },
+        function() {
+          throw new Error('Failed to load Swagger specs.');
+        }
+      );
+    }
+
+    /*
+     * Assigns configuration object to $rootScope
+     * @param config {object} - The configuration object
+    */
+    function assignConfigs (config) {
+      $rootScope.config = config;
+
+      // once configuration is set, get Swagger specs
+      getSwagger();
+    }
+
+    /*
+     * Assigns Swagger specs to $rootScope
+     * @param swagger {object} - the Swagger specs object
+    */
+    function assignSwaggerSpecs(swagger) {
+      $rootScope.swagger = swagger;
+    }
   }
 }
 
-MainCtrl.$inject = ['$http'];
+MainCtrl.$inject = ['$http', '$rootScope'];
 
 export default MainCtrl;
