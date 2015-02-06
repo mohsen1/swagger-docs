bf3749338f7cd7192e52b1c703355783a0608e0c
Mohsen Azimi <msn.azi@gmail.com>
2015-02-05 17:11:52 -0800

    Add nodejs middleware placeholder

---
+++ nodejs/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*
+ * Node.js entry point for Swagger Docs
+*/
+
+module.exports = {
+
+  /*
+   * A connect middle-ware for using Swagger Docs with Express
+  */
+  middleWare: function swaggerDocsMiddleWare() {
+
+    // TODO
+    throw new Error('not yet implemented.');
+  }
+};
