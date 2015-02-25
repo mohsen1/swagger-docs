babb9ad7c00e820ffef38557f8c60b0ebb5b8dd9
Mohsen Azimi <msn.azi@gmail.com>
2015-02-25 09:22:06 -0800

    Add tags rendering

--- src/services/digest.js
+++ src/services/digest.js
@@ -11,13 +11,34 @@ SwaggerDocs.service('Digest', function highlight() {
    * Process Swagger document.
    *
    * @param swagger {object} - A Swagger document object representation
-   * @param callback {function} - Will get resolved with Swagger document object
+   * @param callback {function} - Error first callback function. Will get
+   * resolved with Swagger document object as second argument
    *
   */
   this.process = (swagger, callback)=> {
 
-    const resolve = window.JsonRefs.resolveRefs;
+    if (typeof callback !== 'function') {
+      throw new Error('callback should be a function');
+    }
 
-    resolve(swagger, callback);
+    window.JsonRefs.resolveRefs(swagger, (err, resolved)=> {
+      if (err) {
+        return callback(err);
+      }
+
+      callback(null, applyPathLevelParams(resolved));
+    });
   };
+
+  /*
+   * Apply path level parameters to operations
+   *
+   * @param swagger {object} - A Swagger document object representation
+   * @returns {object} - A Swagger document object representation (same object)
+  */
+  function applyPathLevelParams(swagger) {
+
+    // TODO
+    return swagger;
+  }
 });
