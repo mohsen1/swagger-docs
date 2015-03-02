70e5f9edf1d5d0fea9031781b5426d82c6884ca7
Mohsen Azimi <msn.azi@gmail.com>
2015-03-02 09:59:18 -0800

    Highlight paths too

--- src/app/path/path.controller.js
+++ src/app/path/path.controller.js
@@ -6,6 +6,29 @@ SwaggerDocs.controller('PathCtrl', function PathCtrl($rootScope, $scope, $stateP
   // decode UI components of a path name, it might contain invalid characters
   $scope.pathName = window.decodeURIComponent($stateParams.pathName);
 
+
+  /*
+   * Determines if this path should be highlighted
+   *
+   * @param pathName {string}
+   *
+   * @return {boolean} - true if this operation should be highlighted
+  */
+  $scope.shouldHighlight = function setShouldHighlight(pathName) {
+
+    // if there is an operationName in state, let it highlight the operations
+    return !$stateParams.operationName &&
+
+    // If there is a prameterName in state, let it highlight the parameter
+      !$stateParams.parameterName &&
+
+    // if there is a responseCode in state, let it highlight the response
+      !$stateParams.responseCode &&
+
+    // if state's pathName equals pathName argument
+      $stateParams.pathName === pathName;
+  };
+
   /*
    * filter paths object based on current pathName.
    * If there is no pathName present it will return all paths
