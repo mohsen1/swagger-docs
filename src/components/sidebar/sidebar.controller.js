32ef0643171fe24afc722d2369e2b60eb9402198
Mohsen Azimi <msn.azi@gmail.com>
2015-04-11 21:51:32 -0700

    Implement filter path

--- src/components/sidebar/sidebar.controller.js
+++ src/components/sidebar/sidebar.controller.js
@@ -2,4 +2,17 @@
 
 SwaggerDocs.controller('SidebarCtrl', function SidebarCtrl($scope) {
   $scope.encodeURIComponent = window.encodeURIComponent;
+
+  /*
+   * Determines if the given path should be shown or not based on current filter
+   * @param {string} pathName
+   * @return {boolean} - true if the path should be shown, false otherwise
+  */
+  $scope.showPath = function(pathName, filter='') {
+
+    // Remove whitespace from beginning and end of the filter
+    filter = filter.trim();
+
+    return pathName.indexOf(filter) > -1;
+  };
 });
