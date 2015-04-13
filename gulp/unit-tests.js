a5b42899228a83b06b13ce6570c6099342bec330
Mohsen Azimi <msn.azi@gmail.com>
2015-04-13 09:56:13 -0700

    Fix unit tests (karma.conf.js)

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -17,7 +17,7 @@ function runTests (singleRun) {
   });
 
   var testFiles = bowerDeps.js.concat([
-    paths.tmp + '/serve/app/index.js',
+    paths.tmp + '/serve/{app,components,directives,services}/**/*.js',
     paths.src + '/{app,components,directives,services}/**/*.spec.js',
     paths.src + '/{app,components,directives,services}/**/*.mock.js'
   ]);
