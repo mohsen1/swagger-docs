cfb9ea55521151404961c1fe31e5d12c13984eaa
Mohsen Azimi <msn.azi@gmail.com>
2015-04-13 15:25:30 -0700

    Add main-controller test

--- src/app/main/main.spec.js
+++ src/app/main/main.spec.js
@@ -1,7 +1,20 @@
 'use strict';
 
 describe('MainCtrl', function () {
-  it('should be something', function(){
-    expect(1).toBe(1);
+  var $controller;
+
+  // Load the SwaggerDocs module, which contains the directive
+  beforeEach(module('SwaggerDocs'));
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+  describe('$scope.config', function() {
+    it('$scope.config should not be set initially', function(){
+      var $scope = {};
+      var controller = $controller('MainCtrl', {$scope: $scope});
+      expect($scope.config).toBe(undefined);
+    });
   });
 });
