549fb937d42e881eaac1e63c64c57a76778ba314
Mohsen Azimi <msn.azi@gmail.com>
2015-04-20 18:04:49 -0700

    Fix the middleware

--- nodejs/index.js
+++ nodejs/index.js
@@ -13,11 +13,18 @@ module.exports = {
    * A connect middle-ware for using Swagger Docs with Express
    * @param {object} spec - The Swagger spec document
    * @param {objecg} options - Swagger middleware options
+   * @param {object} config - The SwaggerDocs configuration object
   */
-  middleWare: function swaggerDocs(spec, options) {
+  middleWare: function swaggerDocs(spec, options, config) {
 
     // Swagger Docs static files path
     var docsPath = path.join(__dirname, '..', 'dist');
+
+    // Defaulting config argument
+    if (config === undefined) {
+      config = {loadSwaggerFrom: './swagger.json'};
+    }
+
 
     // Defaulting options argument
     options = options || {};
@@ -30,7 +37,20 @@ module.exports = {
 
       // if request path is stating with options.path serve static content
       if (req.url.indexOf(options.path) === 0) {
-        req.url = req.url.replace(new RegExp('^' + options.path), '');
+        req.url = req.url.replace(new RegExp('^' + options.path), '') || '/';
+
+        // Serve config.json from provided config object
+        if (req.url === '/config.json') {
+          res.setHeader('Content-Type', 'application/json');
+          return res.end(JSON.stringify(config));
+        }
+
+        // Serve swagger.json from provided swagger spec
+        if (req.url === '/swagger.json') {
+          res.setHeader('Content-Type', 'application/json');
+          return res.end(JSON.stringify(spec));
+        }
+
         staticMiddleWare(req, res, next);
 
       // otherwise ignore this request and call next()
